69e9d43eda87a1f7b9d11c626a5034f1566c5bfc
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-16 15:33:31 +0100

    cleaned code and reformatted

--- calculator.js
+++ calculator.js
@@ -6,39 +6,38 @@ const display = document.createElement("div");
 display.style.border = "2px solid black";
 display.style.height = "50px";
 
-for(let i = 0; i < 10; i++){
+for (let i = 0; i < 10; i++) {
     const num = document.createElement("button");
     num.textContent = i;
     main.appendChild(num);
     num.addEventListener("click", function () {
-    display.textContent += i;});
+        display.textContent += i;
+    });
 };
 
 const operators = ["+", "-", "*", "/"];
-for(let i = 0; i < operators.length; i++){
+for (let i = 0; i < operators.length; i++) {
     const oper = document.createElement("button");
     oper.textContent = operators[i];
     main.appendChild(oper);
-    oper.addEventListener("click", function () {display.textContent += operators[i];});
+    oper.addEventListener("click", function () {
+        display.textContent += operators[i];
+    });
 }
 
 const clear = document.createElement("button");
 clear.textContent = "C";
-clear.addEventListener("click", function () {display.textContent = "";});
-
-function safeEval(str){
-    return Function('return ' + str)()
-  }
+clear.addEventListener("click", function () {
+    display.textContent = "";
+});
 
 const equal = document.createElement("button");
 equal.textContent = "=";
-// equal.addEventListener("click",function(){
-//     display.textContent = eval(display.textContent);
-// });
 
-equal.addEventListener("click", function(){
-    display.textContent=Function('return ' + display.textContent)()
-  });
+equal.addEventListener("click", function () {
+    display.textContent = Function('return ' + display.textContent)()
+});
+
 // Append child
 
 main.appendChild(clear);
