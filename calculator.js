40436e296b9779fefefcac329fa45aed77e5a641
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-16 16:49:13 +0100

    created more divs and commented styling attempt in js

--- calculator.js
+++ calculator.js
@@ -4,6 +4,10 @@ const main = document.querySelector("main");
 
 const buttonDiv = document.createElement("div");
 
+const numDiv = document.createElement("div");
+
+const operDiv = document.createElement("div");
+
 const displayDiv = document.createElement("div");
 
 const display = document.createElement("div");
@@ -13,7 +17,7 @@ display.style.height = "50px";
 for (let i = 0; i < 10; i++) {
     const num = document.createElement("button");
     num.textContent = i;
-    buttonDiv.appendChild(num);
+    numDiv.appendChild(num);
     num.addEventListener("click", function () {
         display.textContent += i;
     });
@@ -23,14 +27,14 @@ const operators = ["+", "-", "*", "/"];
 for (let i = 0; i < operators.length; i++) {
     const oper = document.createElement("button");
     oper.textContent = operators[i];
-    buttonDiv.appendChild(oper);
+    operDiv.appendChild(oper);
     oper.addEventListener("click", function () {
         display.textContent += operators[i];
     });
 }
 
 const clear = document.createElement("button");
-clear.textContent = "C";
+clear.textContent = "CE";
 clear.addEventListener("click", function () {
     display.textContent = "";
 });
@@ -43,8 +47,34 @@ equal.addEventListener("click", function () {
 });
 
 // Append child
+
 main.appendChild(buttonDiv);
-buttonDiv.appendChild(clear);
-buttonDiv.appendChild(equal);
+buttonDiv.appendChild(numDiv);
+buttonDiv.appendChild(operDiv);
+operDiv.appendChild(clear);
+numDiv.appendChild(equal);
 main.appendChild(displayDiv);
 displayDiv.appendChild(display);
+
+// ugly js "I use no css" styling section //
+
+// /* Main box styling */
+// main.style.padding = "20%";
+// main.style.display = "flex";
+// main.style.flexDirection = "column-reverse"
+
+// /* Button div styling */
+// buttonDiv.style.width = "25%";
+// buttonDiv.style.length = "25%";
+// buttonDiv.style.padding = "auto";
+// buttonDiv.style.display = "flex";
+// buttonDiv.style.flexWrap = "wrap";
+// buttonDiv.style.flexDirection = "column-reverse";
+
+// /* Num buttons styling */
+// numDiv.style.display="flex";
+// numDiv.style.flexWrap="wrap-reverse";
+
+// /* Display div */
+// displayDiv.style.width = "25%";
+// displayDiv.style.length = "25%";
